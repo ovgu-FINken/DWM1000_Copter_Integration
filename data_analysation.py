136596a3265395297c6bede8b211258f9f3b2f70
jonathan <jonathan.beckhaus@live.de>
2021-07-21 15:06:50 +0200

    irgendwas und so

--- data_analysation.py
+++ data_analysation.py
@@ -2,13 +2,28 @@ import pandas as pd
 import seaborn as sns 
 import matplotlib.pyplot as plt
 import scipy 
+from scipy.stats import norm 
 
+df0 = pd.read_csv("./log_1m.txt", names=["tRound1", "tReply1", "tRound2", "tReply2"])
+df1 = pd.read_csv("./log_1m_no2.txt", names=["tRound1", "tReply1", "tRound2", "tReply2"])
+df2 = pd.read_csv("./log_2m.txt", names=["tRound1", "tReply1", "tRound2", "tReply2"])
+df3 = pd.read_csv("./log_2m_no2.txt", names=["tRound1", "tReply1", "tRound2", "tReply2"])
+df4 = pd.read_csv("./log_3m.txt", names=["tRound1", "tReply1", "tRound2", "tReply2"])
+df5 = pd.read_csv("./log_3m_no2.txt", names=["tRound1", "tReply1", "tRound2", "tReply2"])
+print(df0)
 
-df = pd.read_csv("./log.txt", names=["tRound1", "tReply1", "tRound2", "tReply2"])
-print(df)
+# concat 
+# hue = categorie 
 
-sns.histplot(df["tRound1"]-df["tReply1"])
-g = scipy.stats.normaltest(df["tRound1"]-df["tReply1"])
+#sns.histplot(df0["tRound1"]-df0["tReply1"])
+#sns.histplot(df1["tRound1"]-df1["tReply1"])
+#sns.histplot(df2["tRound1"]-df2["tReply1"])
+#sns.histplot(df3["tRound1"]-df3["tReply1"])
+#sns.histplot(df4["tRound1"]-df4["tReply1"])
+sns.histplot(df5["tRound1"]-df5["tReply1"])
+
+g = scipy.stats.normaltest(df0["tRound1"]-df0["tReply1"])
 print(g)
+mean, var, skew, kurz = norm.stats()
 plt.show()
 
--- serial_logging.py
+++ serial_logging.py
@@ -1,9 +1,12 @@
 import serial 
+from datetime import datetime
 
 ser = serial.Serial("/dev/ttyUSB0", baudrate=115200)
-myfile = open("log_3m_no2.txt", "a")
+myfile = open("times.txt", "a")
 
 for i in range(0, 1001):
     print(f"{i}/1001")
     g = ser.readline()
-    myfile.write(g.decode('utf8'))
+    now = datetime.now().time()
+    print(now)
+    myfile.write(str(now))  #.decode('utf8')

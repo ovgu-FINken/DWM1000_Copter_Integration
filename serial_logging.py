2ffe013440d63a160d44108dfcdd5805cc9462d7
jonathan <jonathan.beckhaus@live.de>
2021-08-04 19:56:42 +0200

    add logs for 1,25m (second one might include cat)

--- serial_logging.py
+++ serial_logging.py
@@ -2,7 +2,7 @@ import serial
 from datetime import datetime
 
 ser = serial.Serial("/dev/ttyUSB0", baudrate=115200)
-myfile = open("times_log_1m_v4.txt", "a")
+myfile = open("times_log_1,25m_v2.txt", "a")
 
 for i in range(0, 6001):
     print(f"{i}/6001")

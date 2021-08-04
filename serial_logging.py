6e0ffccc00557b012d0641d8478a12e0139f0c19
jonathan <jonathan.beckhaus@live.de>
2021-08-04 16:35:59 +0200

    add bigger logs, first one slightly messed up, better ignore it

--- serial_logging.py
+++ serial_logging.py
@@ -2,10 +2,10 @@ import serial
 from datetime import datetime
 
 ser = serial.Serial("/dev/ttyUSB0", baudrate=115200)
-myfile = open("times.txt", "a")
+myfile = open("times_log_1m_v4.txt", "a")
 
-for i in range(0, 1001):
-    print(f"{i}/1001")
+for i in range(0, 6001):
+    print(f"{i}/6001")
     g = ser.readline()
     now = datetime.now().time()
     print(now)

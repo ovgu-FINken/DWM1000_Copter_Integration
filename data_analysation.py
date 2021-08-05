2f65fc7a8f1bc9ad822cafc9f49b1cd3d6378231
jonathan <jonathan.beckhaus@live.de>
2021-08-05 19:58:52 +0200

    add logs with the right content this time

--- data_analysation.py
+++ data_analysation.py
@@ -1,19 +1,19 @@
 import pandas as pd
-import seaborn as sns 
+import seaborn as sns
 import matplotlib.pyplot as plt
-import scipy 
-from scipy.stats import norm 
+import scipy
+from scipy.stats import norm
 
-df0 = pd.read_csv("./log_1m.txt", names=["tRound1", "tReply1", "tRound2", "tReply2"])
-df1 = pd.read_csv("./log_1m_no2.txt", names=["tRound1", "tReply1", "tRound2", "tReply2"])
-df2 = pd.read_csv("./log_2m.txt", names=["tRound1", "tReply1", "tRound2", "tReply2"])
-df3 = pd.read_csv("./log_2m_no2.txt", names=["tRound1", "tReply1", "tRound2", "tReply2"])
-df4 = pd.read_csv("./log_3m.txt", names=["tRound1", "tReply1", "tRound2", "tReply2"])
-df5 = pd.read_csv("./log_3m_no2.txt", names=["tRound1", "tReply1", "tRound2", "tReply2"])
+df0 = pd.read_csv("./times_log_1m_v1.txt", names=["tRound1", "tReply1", "tRound2", "tReply2"])
+df1 = pd.read_csv("./times_log_1m_v2.txt", names=["tRound1", "tReply1", "tRound2", "tReply2"])
+df2 = pd.read_csv("./times_log_1m_v3.txt", names=["tRound1", "tReply1", "tRound2", "tReply2"])
+df3 = pd.read_csv("./times_log_1,25m_v0.txt", names=["tRound1", "tReply1", "tRound2", "tReply2"])
+df4 = pd.read_csv("./times_log_1,25m_v1.txt", names=["tRound1", "tReply1", "tRound2", "tReply2"])
+df5 = pd.read_csv("./times_log_1,25m_v2.txt", names=["tRound1", "tReply1", "tRound2", "tReply2"])
 print(df0)
 
-# concat 
-# hue = categorie 
+# concat
+# hue = categorie
 
 #sns.histplot(df0["tRound1"]-df0["tReply1"])
 #sns.histplot(df1["tRound1"]-df1["tReply1"])
@@ -26,4 +26,3 @@ g = scipy.stats.normaltest(df0["tRound1"]-df0["tReply1"])
 print(g)
 mean, var, skew, kurz = norm.stats()
 plt.show()
-
--- serial_logging.py
+++ serial_logging.py
@@ -1,12 +1,12 @@
-import serial 
+import serial
 from datetime import datetime
 
 ser = serial.Serial("/dev/ttyUSB0", baudrate=115200)
-myfile = open("times_log_1,25m_v2.txt", "a")
+myfile = open("./data/times_2.00m_v0.txt", "a")
 
-for i in range(0, 6001):
-    print(f"{i}/6001")
+for i in range(0, 10001):
+    print(f"{i}/10001")
     g = ser.readline()
     now = datetime.now().time()
-    print(now)
-    myfile.write(str(now))  #.decode('utf8')
+    print(g)
+    myfile.write(g.decode('utf8'))  #

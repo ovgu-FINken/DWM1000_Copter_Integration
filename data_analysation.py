1fee526528974a3aa510ed2d5f6ede48ea8b9b16
jonathan <jonathan.beckhaus@live.de>
2021-08-06 12:58:28 +0200

    match the values with linearization thingy

--- data_analysation.py
+++ data_analysation.py
@@ -5,6 +5,10 @@ import scipy
 from scipy.stats import norm
 import numpy as np
 
+tsfreq = 499.2e6 * 128 # Timestamp counter frequency
+speedOfLight = 299792458.0 # Speed of light in m/s
+MAGIC_RANGE_OFFSET = 153.7 - 0.869707882 + 1.478
+
 df0 = pd.read_csv("./data/times_1m_v0.txt", names=["tRound1", "tReply1", "tRound2", "tReply2"])
 df1 = pd.read_csv("./data/times_1.20m_v0.txt", names=["tRound1", "tReply1", "tRound2", "tReply2"])
 df2 = pd.read_csv("./data/times_1.40m_v0.txt", names=["tRound1", "tReply1", "tRound2", "tReply2"])
@@ -17,36 +21,24 @@ nr = range(0,10001)
 
 g = [df0, df1, df2, df3, df4, df5]
 
+# for df in g:
+#     df["nr"] = nr
+#     plt.figure()
+#     sns.lineplot(y=(df["tRound1"]-df["tReply1"]), x=df0["nr"])
+#     sns.lineplot(y=(df["tRound2"]-df["tReply2"]), x=df0["nr"])
+#     avg = ((df["tRound1"]-df["tReply1"]) + (df["tRound2"]-df["tReply2"]))/2
+#     sns.lineplot(y=avg, x=df0["nr"])
+#     plt.figure()
+#     sns.histplot(avg)
+#     print(np.average(np.array(avg)))
+
+# tPropTick = (double)((tRound1 * tRound2) - (tReply1 * tReply2)) / (tRound1 + tReply1 + tRound2 + tReply2);
 for df in g:
-    df["nr"] = nr
-    plt.figure()
-    sns.lineplot(y=(df["tRound1"]-df["tReply1"]), x=df0["nr"])
-    sns.lineplot(y=(df["tRound2"]-df["tReply2"]), x=df0["nr"])
-    avg = ((df["tRound1"]-df["tReply1"]) + (df["tRound2"]-df["tReply2"]))/2
-    sns.lineplot(y=avg, x=df0["nr"])
-    print(np.average(np.array(avg)))
-
-# plt.figure("1m")
-# sns.histplot(df0["tRound1"][4000:10000]-df0["tReply1"][4000:10000])
-# avg1 = sum(df0["tRound1"][4000:10000]-df0["tReply1"][4000:10000]) / (10000-4000)
-# plt.figure("1.2m")
-# sns.histplot(df1["tRound1"][4000:10000]-df1["tReply1"][4000:10000])
-# avg2 = sum(df1["tRound1"][4000:10000]-df1["tReply1"][4000:10000]) / (10000-4000)
-# plt.figure("1.4m")
-# sns.histplot(df2["tRound1"][4000:10000]-df2["tReply1"][4000:10000])
-# avg3 = sum(df2["tRound1"][4000:10000]-df2["tReply1"][4000:10000]) / (10000-4000)
-# plt.figure("1,6m")
-# sns.histplot(df3["tRound1"][4000:10000]-df3["tReply1"][4000:10000])
-# avg4 = sum(df3["tRound1"][4000:10000]-df3["tReply1"][4000:10000]) / (10000-4000)
-# plt.figure("1.8m")
-# sns.histplot(df4["tRound1"][4000:10000]-df4["tReply1"][4000:10000])
-# avg5 = sum(df4["tRound1"][4000:10000]-df4["tReply1"][4000:10000]) / (10000-4000)
-# plt.figure("2m")
-# sns.histplot(df5["tRound1"][4000:10000]-df5["tReply1"][4000:10000])
-# avg6 = sum(df5["tRound1"][4000:10000]-df5["tReply1"][4000:10000]) / (10000-4000)
-
-
-# g = scipy.stats.normaltest(df0["tRound1"]-df0["tReply1"])
-# print(g)
-# mean, var, skew, kurz = norm.stats()
+    avg = ((df["tRound1"]-df["tReply1"]) + (df["tRound2"]-df["tReply2"]))/4
+    avg = ((speedOfLight * avg / tsfreq) - MAGIC_RANGE_OFFSET ) * 0.90
+    g = scipy.stats.normaltest(avg)
+    print(g)
+    mean = np.average(avg)
+    var = np.var(avg)
+    print(f"mean:{mean} \t var:{var}")
 plt.show()

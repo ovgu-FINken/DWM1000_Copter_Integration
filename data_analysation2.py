cbf7db7dd462cce8536edf157ccd8892ca1f63d4
jonathan <jonathan.beckhaus@live.de>
2021-08-15 13:38:03 +0200

    got some very nice data, + visualization of the recorded clock drifts of 3 different modules

--- data_analysation2.py
+++ data_analysation2.py
@@ -13,21 +13,42 @@ tsfreq = 499.2e6 * 128 # Timestamp counter frequency
 speedOfLight = 299792458.0 # Speed of light in m/s
 MAGIC_RANGE_OFFSET = 153.7 - 0.869707882 + 1.478
 
-df0 = pd.read_csv("./data/sync_error_test2.txt", names=["tStartRound1", "tStartReply1", "tEndReply1", "tEndReply2", "tEndRound2"])
+df0 = pd.read_csv("./data/sync_error_test_boxed_mod3.txt", names=["tStartRound1", "tStartReply1", "tEndReply1", "tEndReply2", "tEndRound2"])
+df1 = pd.read_csv("./data/sync_error_test_boxed_mod2.txt", names=["tStartRound1", "tStartReply1", "tEndReply1", "tEndReply2", "tEndRound2"])
+df2 = pd.read_csv("./data/sync_error_test_boxed_2.txt", names=["tStartRound1", "tStartReply1", "tEndReply1", "tEndReply2", "tEndRound2"])
 print(df0)
 
 time_sync_1 = df0["tStartRound1"]-df0["tStartReply1"]
 time_sync_2 = []
+time_sync_3 = []
+time_sync_4 = []
 for i in range(1, len(df0["tStartRound1"])):
     g = df0["tStartRound1"][i]-df0["tStartRound1"][i-1]
     h = df0["tStartReply1"][i]-df0["tStartReply1"][i-1]
     if((g-h)/g) > 0.1:
         continue
     time_sync_2.append((g-h)/g)
+for i in range(1, len(df1["tStartRound1"])):
+    g = df1["tStartRound1"][i]-df1["tStartRound1"][i-1]
+    h = df1["tStartReply1"][i]-df1["tStartReply1"][i-1]
+    if((g-h)/g) > 0.1:
+        continue
+    time_sync_3.append((g-h)/g)
+for i in range(1, len(df2["tStartRound1"])):
+    g = df2["tStartRound1"][i]-df2["tStartRound1"][i-1]
+    h = df2["tStartReply1"][i]-df2["tStartReply1"][i-1]
+    if((g-h)/g) > 0.1:
+        continue
+    time_sync_4.append((g-h)/g)
+
 
 time_sync_2 = np.array(time_sync_2)
+time_sync_3 = np.array(time_sync_3)
+time_sync_4 = np.array(time_sync_4)
 
 plt.plot(time_sync_2)
+plt.plot(time_sync_3)
+plt.plot(time_sync_4)
 
 print(time_sync_2)
 

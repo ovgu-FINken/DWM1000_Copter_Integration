6f00509ba7a2a8dbc70c0985d92d9f3c76006b05
jonathan <jonathan.beckhaus@live.de>
2021-08-15 14:28:39 +0200

    add another log

--- data_analysation2.py
+++ data_analysation2.py
@@ -16,12 +16,14 @@ MAGIC_RANGE_OFFSET = 153.7 - 0.869707882 + 1.478
 df0 = pd.read_csv("./data/sync_error_test_boxed_mod3.txt", names=["tStartRound1", "tStartReply1", "tEndReply1", "tEndReply2", "tEndRound2"])
 df1 = pd.read_csv("./data/sync_error_test_boxed_mod2.txt", names=["tStartRound1", "tStartReply1", "tEndReply1", "tEndReply2", "tEndRound2"])
 df2 = pd.read_csv("./data/sync_error_test_boxed_2.txt", names=["tStartRound1", "tStartReply1", "tEndReply1", "tEndReply2", "tEndRound2"])
+df3 = pd.read_csv("./data/sync_error_test_boxed_mod4.txt", names=["tStartRound1", "tStartReply1", "tEndReply1", "tEndReply2", "tEndRound2"])
 print(df0)
 
 time_sync_1 = df0["tStartRound1"]-df0["tStartReply1"]
 time_sync_2 = []
 time_sync_3 = []
 time_sync_4 = []
+time_sync_5 = []
 for i in range(1, len(df0["tStartRound1"])):
     g = df0["tStartRound1"][i]-df0["tStartRound1"][i-1]
     h = df0["tStartReply1"][i]-df0["tStartReply1"][i-1]
@@ -41,14 +43,22 @@ for i in range(1, len(df2["tStartRound1"])):
         continue
     time_sync_4.append((g-h)/g)
 
+for i in range(1, len(df3["tStartRound1"])):
+    g = df3["tStartRound1"][i]-df3["tStartRound1"][i-1]
+    h = df3["tStartReply1"][i]-df3["tStartReply1"][i-1]
+    if((g-h)/g) > 0.1:
+        continue
+    time_sync_5.append((g-h)/g)
 
 time_sync_2 = np.array(time_sync_2)
 time_sync_3 = np.array(time_sync_3)
 time_sync_4 = np.array(time_sync_4)
+time_sync_5 = np.array(time_sync_5)
 
 plt.plot(time_sync_2)
 plt.plot(time_sync_3)
 plt.plot(time_sync_4)
+plt.plot(time_sync_5)
 
 print(time_sync_2)
 
